6d5bab54b8f22d82fbc7e8667fdbf9711be2fbb8
Ayush bhatt <124452247+devayush204@users.noreply.github.com>
2023-09-19 22:54:52 +0530

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      
+    <div className="flex justify-center items-center" >
+      <p clasName="text-[40px] font-semibold text-red-600">Heyy there its Ayush... I'm a full stack developer and freelancer.</p>
     </div>
   )
 }
